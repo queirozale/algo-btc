1fea88f622285a8b9eef1c54f6344dabcf3dcfbc
queirozale <queirozalessandro1@gmail.com>
2020-07-09 15:38:20 -0300

    models

--- scripts/algo_btc.py
+++ scripts/algo_btc.py
@@ -102,20 +102,27 @@ class Preprocessing:
         vectors = {'X_train': X[:self.dev_index],
                     'y_train': y[:self.dev_index],
                     'X_test': X[self.dev_index:],
-                    'y_test': y[self.dev_index:]}
+                    'y_test': y[self.dev_index:],
+                    'X': X, 
+                    'y': y}
         return vectors
 
 
 class Modelling:
-    def __init__(self, vectors, minmax):
-        self.X_train, self.y_train = vectors['X_train'], vectors['y_train']
-        self.X_test, self.y_test = vectors['X_test'], vectors['y_test']
+    def __init__(self, vectors, minmax, step):
+        if step == 'validation':
+            self.X_train, self.y_train = vectors['X_train'], vectors['y_train']
+            self.X_test, self.y_test = vectors['X_test'], vectors['y_test']
+        elif step == 'test':
+            self.X_train, self.y_train = vectors['X_train'], vectors['y_train']
+        else:
+            print("You did not input a valid step")
         self.minmax = minmax
+        self.step = step
 
     def LinearReg(self):
         print("==========Starting Linear Regression...==========")
         lr = LinearRegression().fit(self.X_train, self.y_train)
-        print("LinearRegression modelling completed!")
         return lr
 
     def RandomForestReg(self):
@@ -134,29 +141,19 @@ class Modelling:
         rfr_cv = RandomizedSearchCV(rfr, param_dist, cv=6, n_jobs=-1,
                                 verbose=2, n_iter=400, scoring='neg_mean_absolute_error')
         rfr_cv.fit(self.X_train, self.y_train)
-        print("RandomForestRegressor modelling completed!")
         return rfr_cv
-
-    # Moving Average Model
-    def MovingAverage(self):
-        print("==========Starting Moving Average...==========")
-        model_ar = LinearRegression().fit(self.X_train, self.y_train)
-        error = model_ar.predict(self.X_train) - self.y_train
-        model_ma = LinearRegression()
-        model_ma.fit(self.X_train, self.y_train)
-        return model_ma
 
     def MLP(self):
         print("==========Starting Multi-layer Perceptron...==========")
         mlp = Sequential()
         mlp.add(Dense(1, input_dim=7, activation='tanh'))
         mlp.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-        mlp.fit(self.X_train, self.y_train, epochs=10, batch_size=10)
+        mlp.fit(self.X_train, self.y_train, epochs=20, batch_size=10)
         return mlp
 
     def LSTM_net(self):
         print("==========Starting LSTM Network...==========")
-        NEURONS, BATCH_SIZE, NB_EPOCH = 4, 1, 50
+        NEURONS, BATCH_SIZE, NB_EPOCH = 4, 1, 20
         X_train = self.X_train.reshape(self.X_train.shape[0], 1, self.X_train.shape[1])
         model = Sequential()
         model.add(LSTM(NEURONS, batch_input_shape=(BATCH_SIZE, X_train.shape[1], 
@@ -170,52 +167,42 @@ class Modelling:
             print("Epoch {} completed!".format(i+1))
         return model
 
-    def predict(self):
-        lr = self.LinearReg()
-        rfr_cv = self.RandomForestReg()
-        ma = self.MovingAverage()
-        mlp = self.MLP()
-        lstm_net = self.LSTM_net()
+    def predict_val(self):
+        if self.step == 'test':
+            print("step = test, not validation")
+        elif self.step == 'validation':
+            lr = self.LinearReg()
+            rfr_cv = self.RandomForestReg()
+            mlp = self.MLP()
+            lstm_net = self.LSTM_net()
 
-        def get_OriginalScale(values, minmax):
-            valueMin, valueMax = minmax[0], minmax[1]
-            vec_min = np.full(values.shape, valueMin)
-            vec_max = np.full(values.shape, valueMax)
-            return values*(vec_max-vec_min) + vec_min
+            def get_OriginalScale(values, minmax):
+                valueMin, valueMax = minmax[0], minmax[1]
+                vec_min = np.full(values.shape, valueMin)
+                vec_max = np.full(values.shape, valueMax)
+                return values*(vec_max-vec_min) + vec_min
 
-        def get_pred(model):
-            y_pred = get_OriginalScale(model.predict(self.X_test), self.minmax)
-            return y_pred
+            def get_pred(model):
+                y_pred = get_OriginalScale(model.predict(self.X_test), self.minmax)
+                print("Pred finished!")
+                return y_pred
 
-        def calculate_RMSE(y_pred, y_true):
-            RMSE = (np.mean((y_pred - y_true)**2))**0.5
-            return RMSE
+            def calculate_RMSE(y_pred, y_true):
+                RMSE = (np.mean((y_pred - y_true)**2))**0.5
+                return RMSE
 
-        def adj_pred(y_pred, y_true):
-            rmses, ks = [], []
-            for k in range(100):
-                y_pred_ = y_pred + np.full(y_pred.shape, 
-                                        np.mean(y_pred)*k*0.01)
-                rmses.append(calculate_RMSE(y_pred_, y_true))
-                ks.append(k)
-            best_k = ks[rmses.index(min(rmses))]
-            y_pred_ = y_pred + np.full(y_pred.shape, 
-                                    np.mean(y_pred)*best_k*0.01)
-            return y_pred_
-
-        all_pred = {'LinearReg': get_pred(lr),
-                    'RandomForestReg': get_pred(rfr_cv),
-                    'MovingAverage': get_pred(ma),
-                    'MLP': get_pred(mlp),
-                    'LSTM_net': get_OriginalScale(lstm_net.predict(self.X_test.reshape(self.X_test.shape[0], 
-                    1, self.X_test.shape[1])), self.minmax),
-                    'y_true': get_OriginalScale(self.y_test, self.minmax)}
-
-        all_pred['LSTM_net'] = adj_pred(all_pred['LSTM_net'], all_pred['y_true'])
-        
-        return all_pred
-    
-
+            all_pred = {'LinearReg': get_pred(lr),
+                        'RandomForestReg': get_pred(rfr_cv),
+                        'MovingAverageSmoothing': get_OriginalScale(np.mean(self.X_test, axis=1).reshape(-1, 1), self.minmax),
+                        'MLP': get_pred(mlp),
+                        'LSTM_net': get_OriginalScale(lstm_net.predict(self.X_test.reshape(self.X_test.shape[0], 
+                        1, self.X_test.shape[1])), self.minmax),
+                        'y_true': get_OriginalScale(self.y_test, self.minmax)}
+            for key in all_pred.keys():
+                print("RMSE of {}: {}".format(key, round(calculate_RMSE(all_pred[key], all_pred['y_true']), 2)))
+            return all_pred
+        else:
+            print("You did not input a valid step")
 
 # References
 # Hyndman, R.J., & Athanasopoulos, G. (2018) Forecasting: principles and practice, 2nd edition, OTexts: Melbourne, Australia. OTexts.com/fpp2. Accessed on <07/07/2020>.
--- scripts/init.py
+++ scripts/init.py
@@ -1,5 +1,6 @@
 from algo_btc import LoadData, Preprocessing, Modelling
 import matplotlib.pyplot as plt
+import pickle
 
 def main():
     STOCK_NAME = "BTC-USD"
@@ -8,19 +9,35 @@ def main():
     pp = Preprocessing(ld.df.copy(), 0.1)
     minmax = pp.get_minmax()
     vectors = pp.sampling()
-    X_train, y_train = vectors['X_train'], vectors['y_train']
-    X_test, y_test = vectors['X_test'], vectors['y_test']
-    m = Modelling(vectors, minmax)
-    all_pred = m.predict()
-    dates = ld.df['Date'][int(len(ld.df)*0.9)+7:]
-    fig = plt.figure(figsize=[16, 5])
-    for key in all_pred.keys():
-        plt.plot(dates, all_pred[key], label=key)
-    plt.legend()
-    plt.title('Validation sample predictions')
-    plt.savefig('val_pred.png')
-    plt.show()
 
+    def run_validation():
+        m = Modelling(vectors, minmax, 'validation')
+        all_pred = m.predict_val()
+        dates = ld.df['Date'][int(len(ld.df)*0.9)+7:]
+        fig = plt.figure(figsize=[16, 5])
+        for key in all_pred.keys():
+            plt.plot(dates, all_pred[key], label=key)
+        plt.legend()
+        plt.title('Validation sample predictions')
+        plt.savefig('val_pred.png')
+        plt.show()
+
+    def train_model():
+        m = Modelling(vectors, minmax, 'test')
+        # Creating the models
+        lr = m.LinearReg()
+        rfr_cv = m.RandomForestReg()
+        mlp = m.MLP()
+        lstm_net = m.LSTM_net()
+        # Saving the models
+        with open('models/lr.pkl', 'wb') as f:
+            pickle.dump(lr, f)
+        with open('models/rfr_cv.pkl', 'wb') as f:
+            pickle.dump(rfr_cv, f)
+        mlp.save('models/mlp.h5')
+        lstm_net.save('models/lstm_net.h5')
+    
+    train_model()
 
 if __name__ == "__main__":
     main()
